98ecfa062494345e4a0eedad1626f44bfe8212a8
Ujjawal Verma <ujjawal.verma9651@gmail.com>
2023-12-31 10:40:08 +0530

    Some Minor design changes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useLocation } from 'react-router-dom'
+import { useScrollPosition } from '../hooks/userScrollPosition.js';
 const Navigation = () => {
+  let location = useLocation();
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+  const scrollPosition = useScrollPosition();
   return (
-    <div><div className="flex justify-evenly text-2xl mb-12 p-4 bg-black text-white fixed w-[100%] z-10 opacity-30"> 
-    <div><Link to='/'>HOME</Link></div>
+    <div><div className={classNames(scrollPosition>20?"flex justify-evenly transition-all bg-white text-2xl mb-12 p-2  fixed w-[100%] z-10 ":"flex justify-evenly text-2xl mb-12 p-2  fixed w-[100%] z-10 ")}> 
+    <div className={(location.pathname==='/')?'bg-black text-white px-3 py-2':''}> <Link to='/'>HOME</Link></div>
     <div>Projects</div>
-    <div><Link to='/post'>Posts/Blogs</Link></div>
+    <div className={(location.pathname==='/post')?'bg-black text-white px-3 py-2':''}><Link to='/post' >Posts/Blogs</Link></div>
+   
+   
   </div></div>
   )
+  
 }
 
 export default Navigation
