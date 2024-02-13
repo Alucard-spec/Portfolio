2f1ae54c6b4fb28344b90343653ad9d1dc681684
Ujjawal Verma <ujjawal.verma9651@gmail.com>
2024-02-13 20:06:46 +0530

    fixes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
 A clone of one of the most popular streaming websites spotify. <br />Made with the help of Next-js , Tailwind and Supabase . <br />
 Contains various functinalities like User Authentication, Song Upload , Liked Playlist and ofcourse song playback. <br />
 <br />
-<iframe src="https://spotify-clone-oydb.vercel.app/" frameborder="0" title='spotify-clone'></iframe>
+<iframe src="https://spotify-clone-oydb.vercel.app/" frameborder="0" title='spotify-clone' height={400}></iframe>
 <span className='font-bold'> Live Website</span> -{'>'} <a href="https://spotify-clone-oydb.vercel.app/" className='text-purple-800 font-bold hover:bg-white p-3'>Spotify-Clone</a>
 </div>
     </div></div>
