3a2ceeb2a79d359b913861420d67c17978e750ce
Ujjawal <109836795+Alucard-spec@users.noreply.github.com>
2024-02-13 18:40:14 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,9 @@ import Contact from './Contact.jsx'
 const Home = () => {
   return (
     <div className='md:text-center pt-36'>
-      <div className='flex flex-row-reverse justify-evenly'>
-      <img src={require("./Content Writing/avatar.png")} className='flex-none w-64 h-64' alt="Avatar" />
-<div className="text-8xl font-bold md:text-4xl lg:text-5xl  w-fit py-12 px-0 box-border ">
+      <div className='flex flex-row-reverse justify-evenly md:flex-col'>
+      <img src={require("./Content Writing/avatar.png")} className='md:mx-auto flex-none w-64 h-64' alt="Avatar" />
+<div className="text-8xl md:mx-auto font-bold md:text-4xl lg:text-5xl  w-fit py-12 px-0 box-border ">
 
 Ujjawal Verma
 </div></div>
