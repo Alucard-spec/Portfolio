a8d409c05893550d54b8fbf0d0c274e1d814bb75
Ujjawal <109836795+Alucard-spec@users.noreply.github.com>
2024-02-13 18:33:04 +0530

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ const AboutMe = () => {
   return (
     <div className='m-12 py-14 flex  md:flex-col justify-evenly ' >
        <div className="bg-white font-bold text-3xl p-4 rounded-3xl h-fit my-auto md:mx-auto w-fit">About Me</div>
-
+<br/>
        
         <div className='w-[50%] md:w-full text-2xl font-semibold  '>
 Passionate about code and captivated by the ever-evolving tech landscape. I thrive on turning ideas into reality through clean, efficient, and innovative solutions. Let's craft the future, one line of code at a time!
