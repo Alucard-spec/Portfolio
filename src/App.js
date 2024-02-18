6ce2cbfae6409be785249b478903dcc5cdd60e7a
Ujjawal Verma <ujjawal.verma9651@gmail.com>
2024-02-18 17:10:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 
-import { Route, Routes } from 'react-router-dom';
-
+import { Route, Routes,useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from "./components/Home.jsx";
 import Navbar from './components/Navbar.jsx';
 import Projects from './components/Projects.jsx';
@@ -10,6 +10,11 @@ import ExtendedAboutMe from './components/ExtendedAboutMe.jsx';
 
 
 function App() {
+
+  const {pathname}=useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   const bodyStyle = {
     backgroundColor: '#ffbe0b',color:'#000814',margin:0,padding:0,minHeight:'100vh',fontFamily:"Anta, sans-serif",fontWeight:400,fontStyle:"normal"
   };
