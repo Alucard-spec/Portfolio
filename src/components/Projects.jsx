f9ca71003b21741ad0aec73c8e2c3c792e2880a2
Ujjawal Verma <ujjawal.verma9651@gmail.com>
2024-07-31 13:31:45 +0530

    Add School Website and Change descriptions of Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
       <br />
        
         <div className='flex gap-16 lg:gap-6 md:gap-1 md:flex-col w-full px-16 lg:px-12 md:px-3 text-2xl md:text-xl italic items-center   '><div className='h-fit w-[60%] md:w-full my-auto'>
-        Created a clone of Spotify using Next.js, Tailwind CSS, and Supabase. The project includes various functionalities such as user authentication, song upload, liked playlists, and song playback.<br />
+        Created a lively Spotify clone using Next.js, Tailwind CSS, and Supabase, bringing music streaming to life! This project includes a range of exciting features such as user authentication, song uploads, liked playlists, and seamless song playback, making it a joy to explore and enjoy your favorite tunes.<br />
 <a href="https://spotify-clone-oydb.vercel.app/" className='text-blue-500 font-bold hover:bg-white p-1 rounded-xl'>Github Repo</a> </div>
 <br /><div className='w-[40%] md:w-full'>
 <img className=' object-cover mx-auto ' alt='project sample' src={require("./Content Writing/Spotify_clone.png")}  /></div>
@@ -26,7 +26,7 @@ const Projects = () => {
       <br />
        
         <div className='flex gap-16 lg:gap-6 md:gap-1 md:flex-col  px-16 lg:px-12 md:px-3 text-2xl md:text-xl items-center italic   '><div className='h-fit w-[60%] md:w-full my-auto'>
-        Created a gamified to-do application where users could add tasks and assign them difficulty levels. Tasks with higher difficulty levels yielded higher scores upon completion, contributing to the user's overall score. Upon accumulating enough score, users advanced to higher ranks or levels, adding a competitive and rewarding element to task management.<br />
+        Created a gamified to-do application that turns task management into an exciting adventure! Users can add tasks and assign them difficulty levels, with tougher tasks yielding higher scores. As users complete tasks and rack up points, they advance to higher ranks or levels, making productivity fun and rewarding.<br />
         <a href="https://github.com/Alucard-spec/RPL-Todo" className='text-blue-500 font-bold hover:bg-black p-1 rounded-xl'>Github Repo</a>  </div>
 <br /><div className='w-[40%] md:w-full'>
 <img className='object-cover mx-auto ' alt='Project sample' src={require("./Content Writing/RPL.png")}  /></div>
@@ -41,7 +41,7 @@ const Projects = () => {
       <br />
        
         <div className='flex gap-16 lg:gap-6 md:gap-1 md:flex-col  px-16 lg:px-12 md:px-3 text-2xl md:text-xl items-center italic   '><div className='h-fit w-[60%] md:w-full my-auto'>
-        Created a platformer video game using Godot, featuring two exciting levels. The game utilizes free assets from itch.io. Players are tasked with collecting as many strawberries as possible while navigating through the levels to reach the finish line.<br />
+        Created a platformer video game using Godot, featuring two levels filled with strawberries. The game, adorned with vibrant free assets from itch.io, invites players to collect as many juicy strawberries as possible while navigating through the levels to reach the finish line.<br />
         <a href="https://github.com/Alucard-spec/platformer" className='text-blue-500 font-bold hover:bg-white p-1 rounded-xl'>Github Repo</a>  </div>
 <br /><div className='w-[40%] md:w-full'>
 <img className='object-cover mx-auto ' alt='Project sample' src={require("./Content Writing/Platformer.png")}  /></div>
@@ -56,7 +56,7 @@ const Projects = () => {
       <br />
        
         <div className='flex gap-16 lg:gap-6 md:gap-1 md:flex-col  px-16 lg:px-12 md:px-3 text-2xl md:text-xl items-center italic   '><div className='h-fit w-[60%] md:w-full my-auto'>
-        Created a typing test web app to measure typing speed in words per minute. The project was built using React.js and Tailwind CSS for a smooth and responsive user experience.<br />
+        Created a fun and engaging typing test web app that measures typing speed in words per minute. Built with React.js and Tailwind CSS, the app delivers a smooth and responsive user experience, making the challenge of improving your typing skills both enjoyable and rewarding.<br />
         <a href="https://github.com/Alucard-spec/Type-Warrior" className='text-blue-500 font-bold hover:bg-black p-1 rounded-xl'>Github Repo</a>  </div>
 <br /><div className='w-[40%] md:w-full'>
 <img className='object-cover mx-auto ' alt='Project sample' src={require("./Content Writing/Type_warrior.png")}  /></div>
@@ -64,7 +64,21 @@ const Projects = () => {
 </div>
     </div>
 
-    
+    {/* School Website */}
+
+    <div className=' py-14 bg-black text-white  flex  flex-col justify-evenly border-2 border-black ' >
+       <div className=" font-bold text-5xl lg:text-4xl md:text-3xl px-16 lg:px-12 md:px-3  text-left  "> <span className='border-b-4 border-yellow-400'>School Website</span></div>
+      <br />
+       
+        <div className='flex gap-16 lg:gap-6 md:gap-1 md:flex-col  px-16 lg:px-12 md:px-3 text-2xl md:text-xl items-center italic   '><div className='h-fit w-[60%] md:w-full my-auto'>
+        Created a vibrant and interactive school website that showcases the school's features and curriculum using randomly generated data. The site is packed with fun functionalities like a gallery with filters, a lively carousel, and user-friendly forms. Using React.js for seamless front-end development and Tailwind CSS for a sleek, modern design, this project was a joy to build and brings the school's offerings to life.<br />
+        <a href="https://github.com/Alucard-spec/platformer" className='text-blue-500 font-bold hover:bg-white p-1 rounded-xl'>Github Repo</a>  </div>
+<br /><div className='w-[40%] md:w-full'>
+<img className='object-cover mx-auto ' alt='Project sample' src={require("./Content Writing/School_website.png")}  /></div>
+
+</div>
+    </div>
+   
 
     </div>
   )
